fda975c32ffc86833bb03e1b9821f34e1957f763
kevinseq1 <kevinseq1@gmail.com>
2023-01-03 22:53:16 -0800

    Add MVC design patter ⠵

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,31 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        // Gets the title of the button pressed
+        let userAnswer = sender.currentTitle!
+        
+        // Checks if the user chose choice1 or choice2
+        storyBrain.nextStory(userAnswer)
+        
+        // Update the UI after the user chooses an option
+        updateUI()
+    
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getStoryChoice1(), for: [])
+        choice2Button.setTitle(storyBrain.getStoryChoice2(), for: [])
+    }
+    
 }
 
